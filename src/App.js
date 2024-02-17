e4bca5ca83b22e19a168af004f5c9d98c8fad43a
Ayush Panchal <ayushrockz11@gmail.com>
2024-02-17 12:29:02 +0530

    Changes accumulated over time

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Routes, Route,} from 'react-router-dom';
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Dashboard from './pages/Dashboard'
+import Homepage from './pages/Homepage'
+import Notifs from './pages/Notifs'
+import SubmitInternship from './pages/SubmitInternship'
+import AdminDashboard from './pages/AdminDashboard'
+import AdminLogin from './pages/AdminLogin'
+import AdminPostNotifs from './pages/AdminPostNotifs'
+import AdminNotifs from './pages/AdminNotifs'
+import AdminSearch from './pages/AdminSearch'
 
-function App() {
+import 'bootstrap/dist/css/bootstrap.css';
+import { AuthProvider, RequireAuth } from 'react-auth-kit';
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AuthProvider
+        authType={"cookie"}
+        authName={"_auth"}
+        cookieDomain={window.location.hostname}
+        cookieSecure={false}
+      >
+        <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Homepage/>} />
+          <Route path="/login" exact element={<Login/>} />
+          <Route path="/adminlogin" exact element={<AdminLogin/>} />
+          <Route path="/register" exact element={<Register/>} />
+          <Route path="/dashboard" exact element={<RequireAuth loginPath='/login'><Dashboard/></RequireAuth>} />
+          <Route path="/notifications" exact element={<RequireAuth loginPath='/login'><Notifs/></RequireAuth>} />
+          <Route path="/submitinternship" exact element={<RequireAuth loginPath='/login'><SubmitInternship/></RequireAuth>} />
+          <Route path="/admindashboard" exact element={<RequireAuth loginPath='/adminlogin'><AdminDashboard/></RequireAuth>} />
+          <Route path="/adminpostnotifs" exact element={<RequireAuth loginPath='/adminlogin'><AdminPostNotifs/></RequireAuth>} />
+          <Route path="/adminnotifs" exact element={<RequireAuth loginPath='/adminlogin'><AdminNotifs/></RequireAuth>} />
+          <Route path="/adminsearch" exact element={<RequireAuth loginPath='/adminlogin'><AdminSearch/></RequireAuth>} />
+        </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
